76954365644df442614807cb59ba7cc38bd4a846
Fabio Pelosin <fabiopelosin@gmail.com>
2014-05-21 11:43:54 +0200

    Add CFASecrets.h example

---
+++ objc/Confab App/CFASecrets_example.h
@@ -0,0 +1,16 @@
+//
+//  CFASecrets.h
+//  Confab App
+//
+//  Created by Fabio Pelosin on 10/11/13.
+//  Copyright (c) 2013 Fabio Angelog Pelosin. All rights reserved.
+//
+
+#ifndef Confab_App_CFASecrets_h
+#define Confab_App_CFASecrets_h
+
+#define kCFAClientID @"VALUE"
+#define kCFAClientSecret @"VALUE"
+#define kCFARedirectURI @"confabapp://auth"
+
+#endif
